a8db03dd294c7a49fb7912920a122a07a9aa64e9
Parson <35521194+Alanstia@users.noreply.github.com>
2018-06-18 16:35:01 +0800

    Update luis.js

--- luis.js
+++ luis.js
@@ -20,11 +20,64 @@ function getLuisIntent(utterance,callback) {
         "verbose":  true,
         "q": utterance
     }
-
+	
+	var goods = {起士漢堡:50,奶茶:40}; //菜單
+	var price = 0; //總金額
     var luisRequest =endpoint + luisAppId +'?' + querystring.stringify(queryParams);
+	//中文數字轉成int
+	var chnNumChar = {
+		零:0,
+		一:1,
+		二:2,
+		三:3,
+		四:4,
+		五:5,
+		六:6,
+		七:7,
+		八:8,
+		九:9,
+		兩:2
+	};
+	//中文數字轉成int
+	var chnNameValue = {
+		十:{value:10, secUnit:false},
+		百:{value:100, secUnit:false},
+		千:{value:1000, secUnit:false},
+		萬:{value:10000, secUnit:true},
+		億:{value:100000000, secUnit:true}
+	}
+	//中文數字轉成int
+	function ChineseToNumber(chnStr){
+		var rtn = 0;
+		var section = 0;
+		var number = 0;
+		var secUnit = false;
+		var str = chnStr.split('');
 
+		for(var i = 0; i < str.length; i++){
+			var num = chnNumChar[str[i]];
+			if(typeof num !== 'undefined'){
+				number = num;
+				if(i === str.length - 1){
+					section += number;
+				}
+			}else{
+				var unit = chnNameValue[str[i]].value;
+				secUnit = chnNameValue[str[i]].secUnit;
+				if(secUnit){
+					section = (section + number) * unit;
+					rtn += section;
+					section = 0;
+				}else{
+					section += (number * unit);
+				}
+				number = 0;
+			}
+		}
+		return rtn + section;
+	}
 
-
+	
     request(luisRequest,
         function (err,
             response, body) {
@@ -36,16 +89,29 @@ function getLuisIntent(utterance,callback) {
                 console.log(`Query: ${data.query}`);
                 console.log(`Top Intent: ${data.topScoringIntent.intent}`);
                 if(data.topScoringIntent.intent=='打招呼'){
-                    callback('您好，我是人工智慧點餐系統，代號為002，很高興為您服務!');
+                    callback('您好，我是人工智慧點餐系統，代號為002，很高興為您服務!',price,false);
                 }
                 else if(data.topScoringIntent.intent=='點餐'){
-                    callback('好的，已為您點了'+data.entities[0].entity+data.entities[1].entity);
+					if(data.entities[0].entity != null && data.entities[1].entity != null && data.entities[2].entity != null)
+					{
+						var int_quantity = ChineseToNumber(data.entities[1].entity); //中文數字轉成int
+						callback('好的，已為您點了'+data.entities[1].entity+data.entities[2].entity+data.entities[0].entity,price,false);
+						if(typeof goods[data.entities[0].entity] !== 'undefined') //計算金額
+						{
+							price = price + (goods[data.entities[0].entity]*int_quantity);
+							callback('您點了'+goods[data.entities[0].entity]*int_quantity+'元',price,true);
+						}
+					}
+					else
+					{
+						callback('抱歉!請您說清楚一點，請附上數量(一個,三杯...etc)',price,false);
+					}
                 }
                 else if(data.topScoringIntent.intent=='結帳'){
-                    callback('謝謝惠顧');
+                    callback('謝謝惠顧',price,false);
                 }
                 else if(data.topScoringIntent.intent=='None'){
-                    callback("不好意思，我目前聽不懂這個詞呢");
+                    callback("不好意思，請您說清楚一點",price,false);
                 }
                 
                 
