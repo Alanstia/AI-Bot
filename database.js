4bbd41ced2ed1b7c1f5380561a53034c1058c21b
jessetsaoj <37109530+jessetsaoj@users.noreply.github.com>
2018-06-21 19:12:06 +0800

    新增Database功能，與Mysql連接

---
+++ database.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql');
+const conn = mysql.createConnection({
+    host     : '140.138.77.98',
+    user     : 'yzujava201609',
+    password : 'm0654284vm,6java',
+    database : '2017_JAVA'
+});
+
+
+module.exports = {
+    getMenu(callback) {
+        conn.connect();
+        var menu = new Array();
+        conn.query('SELECT item_id, item_name, item_price FROM AIBotMenu', (error, results, fields)=> {
+            if(error) {
+                console.log(error.message)
+                return ;
+            }
+            results.forEach((element) => {
+                menu.push(element);
+            });
+            
+            callback(menu);
+        });
+    }
+}
+
